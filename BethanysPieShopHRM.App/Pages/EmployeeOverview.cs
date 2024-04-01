de65dce79d5ade86aabeb7b3e159b028d1c1c2e7
Coleman Buffa <coleman.buffa@gmail.com>
2024-03-31 22:57:44 -0700

    Module 4

--- BethanysPieShopHRM.App/Pages/EmployeeOverview.cs
+++ BethanysPieShopHRM.App/Pages/EmployeeOverview.cs
@@ -1,7 +1,7 @@
-﻿using BethanysPieShopHRM.App.Services;
+﻿using BethanysPieShopHRM.App.Components;
+using BethanysPieShopHRM.App.Services;
 using BethanysPieShopHRM.Shared;
 using Microsoft.AspNetCore.Components;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,14 +10,30 @@ namespace BethanysPieShopHRM.App.Pages
 {
     public partial class EmployeeOverview
     {
+        public IEnumerable<Employee> Employees { get; set; }
+
         [Inject]
         public IEmployeeDataService EmployeeDataService { get; set; }
 
-        public IEnumerable<Employee> Employees { get; set; }
+        protected AddEmployeeDialog AddEmployeeDialog { get; set; }
 
-        protected override async Task OnInitializedAsync()
+        protected async override Task OnInitializedAsync()
+        {
+
+            Employees = (await EmployeeDataService.GetAllEmployees()).ToList();
+
+           
+        }
+
+        protected void QuickAddEmployee()
+        {
+            AddEmployeeDialog.Show();
+        }
+
+        public async void AddEmployeeDialog_OnDialogClose()
         {
             Employees = (await EmployeeDataService.GetAllEmployees()).ToList();
+            StateHasChanged();
         }
     }
 }
